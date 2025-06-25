ff811ba8590b532c4ef0799eb3b7de2fc6181f18
Noah rakoto <noahrakoto342@gmail.com>
2025-06-25 11:23:50 +0200

    vita

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -49,7 +49,11 @@ function fiche_employe($emp_no) {
     $conn = dbconnect();
     $req = "SELECT e.emp_no, e.last_name, e.first_name, e.birth_date, e.hire_date, d.dept_name FROM employees e JOIN dept_emp de ON e.emp_no = de.emp_no JOIN departments d ON de.dept_no = d.dept_no WHERE e.emp_no = '$emp_no'";
     $result = mysqli_query($conn, $req);
-    return mysqli_fetch_assoc($result);
+    $formulaire = [];
+    if ($row = mysqli_fetch_assoc($result)) {
+        $formulaire = $row;
+    }
+    return $formulaire;
 }
 function salary_history($emp_no) {
     $conn = dbconnect();
