d05476069a2630af0eaacc3b45e0ec3faf98dc65
Noah rakoto <noahrakoto342@gmail.com>
2025-06-25 11:19:42 +0200

    uptadate fucntion

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -33,4 +33,33 @@ function afficher_current_manager($current)
     }
     return $managers;
 }
+function Formulaire($departements , $current , $ageMin , $ageMax)
+{
+    $conn = dbconnect();
+    $req = "SELECT e.emp_no, e.last_name, e.first_name,d.dept_name FROM employees e JOIN dept_emp de ON e.emp_no = de.emp_no WHERE de.dept_no = '$departements' AND e.birth_date BETWEEN DATE_SUB(NOW(), INTERVAL $ageMax YEAR) AND DATE_SUB(NOW(), INTERVAL $ageMin YEAR) AND e.firts_name == $current";
+    $result = mysqli_query($conn, $req);
+    $employes = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $employes[] = $row;
+    }
+    return $employes;
+
+}
+function fiche_employe($emp_no) {
+    $conn = dbconnect();
+    $req = "SELECT e.emp_no, e.last_name, e.first_name, e.birth_date, e.hire_date, d.dept_name FROM employees e JOIN dept_emp de ON e.emp_no = de.emp_no JOIN departments d ON de.dept_no = d.dept_no WHERE e.emp_no = '$emp_no'";
+    $result = mysqli_query($conn, $req);
+    return mysqli_fetch_assoc($result);
+}
+function salary_history($emp_no) {
+    $conn = dbconnect();
+    $req = "SELECT s.salary, s.from_date, s.to_date FROM salaries s WHERE s.emp_no = '$emp_no' ORDER BY s.from_date DESC";
+    $result = mysqli_query($conn, $req);
+    $salaries = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $salaries[] = $row;
+    }
+    return $salaries;
+}
+
 ?>
