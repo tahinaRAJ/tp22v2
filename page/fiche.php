2412275f7ab9d063d3d08926af04e700e44786da
tahinaRAJ <tahinaraj1235@gmail.com>
2025-06-30 12:57:56 +0200

    next

--- page/fiche.php
+++ page/fiche.php
@@ -2,6 +2,7 @@
 require("../inc/fonctions.php");
 $fiche = fiche_employe($_GET['code']);
 $salaire = salary_history($_GET['code']);
+$emplois = afficher_emploi($_GET['code']);
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +14,7 @@ $salaire = salary_history($_GET['code']);
     <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <header>Fiche de l'employé</header>
+    <header><h1>Fiche de l'employé</h1></header>
         <main>
             <table class="table table-bordered table-striped" id="ficheTable">
                 <thead>
@@ -22,6 +23,7 @@ $salaire = salary_history($_GET['code']);
                         <th>Nom</th>
                         <th>Prénom</th>
                         <th>Date de naissance</th>
+                        <th>Genre</th>
                         <th>Date d'embauche</th>
                         <th>Département</th>
                     </tr>
@@ -32,6 +34,7 @@ $salaire = salary_history($_GET['code']);
                         <td><?= htmlspecialchars($fiche['last_name']) ?></td>
                         <td><?= htmlspecialchars($fiche['first_name']) ?></td>
                         <td><?= htmlspecialchars($fiche['birth_date']) ?></td>
+                        <td><?= htmlspecialchars($fiche['gender']) ?></td>
                         <td><?= htmlspecialchars($fiche['hire_date']) ?></td>
                         <td><?= htmlspecialchars($fiche['dept_name']) ?></td>
                     </tr>
@@ -39,6 +42,7 @@ $salaire = salary_history($_GET['code']);
             </table>
             <table class="table table-bordered table-striped" id="salaryTable">
                 <thead>
+                    <h1>Historique des salaires</h1>
                     <tr>
                         <th>Salaire</th>
                         <th>Date de début</th>
@@ -55,6 +59,25 @@ $salaire = salary_history($_GET['code']);
                     <?php endforeach; ?>
                 </tbody>
                  </table>
+            <table class="table table-bordered table-striped" id="emploiTable">
+                <thead>
+                    <h1>Emploi</h1>
+                    <tr>
+                        <th>Poste</th>
+                        <th>Date de début</th>
+                        <th>Date de fin</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($emplois as $empl): ?>
+                    <tr>
+                        <td><?= htmlspecialchars($empl['title']) ?></td>
+                        <td><?= htmlspecialchars($empl['from_date']) ?></td>
+                        <td><?= htmlspecialchars($empl['to_date']) ?></td>
+                    </tr>
+                    <?php endforeach; ?>
+                    
+                </tbody>
             </main>
 </body>
 </html>
